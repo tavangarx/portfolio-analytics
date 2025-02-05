e1daba7f5bf3fc45a0b0ee605fcd91565622a13e
Mohammad Hossein Tavangar <91421224+tavangarx@users.noreply.github.com>
2025-02-05 12:53:56 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Integrations from './components/Integrations';
 import Footer from './components/Footer';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <main className="flex-grow">
         <Hero />
+        <Integrations />
       </main>
       <Footer />
     </div>
