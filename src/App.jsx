06599453145063488344091affa5943576fad552
Mohammad Hossein Tavangar <91421224+tavangarx@users.noreply.github.com>
2025-02-05 12:26:11 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Header from './components/Header';
 import Hero from './components/Hero';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="min-h-screen">
-      <Hero />
+    <div className="min-h-screen flex flex-col bg-white">
+      <Header />
+      <main className="flex-grow">
+        <Hero />
+      </main>
+      <Footer />
     </div>
   );
 }
